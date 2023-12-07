0b123f97d5af37406c67b9a2e086222a5fee4781
Ben <138155842+10aiengineer01@users.noreply.github.com>
2023-12-07 21:24:14 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,42 @@
+let names = JSON.parse(localStorage.getItem('names')) || [];
+let currentNameIndex = 0;
+var randomIndex = 0;
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadRandomCard();
+    document.querySelector('.card-container').addEventListener('click', flipCard);
+    // Keine Notwendigkeit, die Namenanzeige hier zu initialisieren
+});
+
 function flipCard() {
     var card = document.querySelector('.card');
-    card.classList.add('flipped'); // Startet die Flip-Animation
-    
-    // Warte auf das Ende der Animation (600ms)
+    var nameDisplayContainer = document.getElementById('nameDisplayContainer');
+    var nameDisplay = document.getElementById('nameDisplay');
+
+    card.classList.add('flipped'); // Start the flip animation
+
+    // Start the name animation
+    if (names.length > 0) {
+        var randomIndex1 = Math.floor(Math.random() * names.length);
+        while (randomIndex == randomIndex1) {
+            randomIndex = Math.floor(Math.random() * names.length);
+        }
+        randomIndex = randomIndex1
+        updateNameDisplay(names[randomIndex1]);
+    }
+
+    card.classList.add('flipped'); // Starte die Umdreh-Animation
+
+    // Mache den Namen sichtbar und bewege ihn an die Spitze
+    nameDisplayContainer.style.transform = 'translateX(-50%) translateY(0)';
+    nameDisplayContainer.style.opacity = '1';
+    nameDisplayContainer.style.visibility = 'visible';
+
+    // Wait for the end of the flip animation
     setTimeout(function() {
-        loadRandomCard(); // Lade eine neue Karte, wenn die Animation endet
-        card.classList.remove('flipped'); // Setzt die Flip-Animation zurück
-    }, 600);
+        loadRandomCard(); // Load a new card when the animation ends
+        card.classList.remove('flipped'); // Reset the flip animation
+    }, 600); // Adjusted timing for the card flip
 }
 
 function loadRandomCard() {
@@ -15,12 +45,7 @@ function loadRandomCard() {
     cardImage.src = 'cards/' + randomCardNumber + '.png';
 }
 
-document.addEventListener('DOMContentLoaded', loadRandomCard);
-
-// Korrigiert von 'getSelector' zu 'querySelector'
-document.querySelector('.card-container').addEventListener('click', flipCard);
-
-document.addEventListener('DOMContentLoaded', function() {
-    var names = JSON.parse(localStorage.getItem('names'));
-    // Machen Sie etwas mit den Namen, z.B. anzeigen in einer Liste
-});
+function updateNameDisplay(name) {
+    var nameDisplay = document.getElementById('nameDisplay');
+    nameDisplay.textContent = name; // Setzt den aktuellen Namen
+}
