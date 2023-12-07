ee0e56ed3aea36571adaa6b6c74a209dc965d74a
Ben <138155842+10aiengineer01@users.noreply.github.com>
2023-12-07 21:24:42 +0100

    Update start.js

--- start.js
+++ start.js
@@ -9,6 +9,7 @@ document.getElementById('saveButton').addEventListener('click', function() {
     var name = document.getElementById('nameInput').value;
     if(name) {
         names.push(name); // Fügt den Namen der Liste hinzu
+        updateNameList(); // Aktualisiert die Liste der Namen
         document.getElementById('nameInput').value = ''; // Setzt das Eingabefeld zurück
         document.getElementById('continueButton').classList.remove('hidden'); // Zeigt den "Weiter"-Button an
     }
@@ -16,5 +17,26 @@ document.getElementById('saveButton').addEventListener('click', function() {
 
 document.getElementById('continueButton').addEventListener('click', function() {
     localStorage.setItem('names', JSON.stringify(names)); // Speichert die Liste im Local Storage
-    window.location.href = 'index.html'; // Weiterleitung zur Hauptseite
+    window.location.href = 'startpage.html'; // Weiterleitung zur Hauptseite
 });
+
+function updateNameList() {
+    const list = document.getElementById('nameList');
+    list.innerHTML = ''; // Leert die Liste
+    names.forEach(function(name) {
+        const listItem = document.createElement('li');
+        listItem.textContent = name;
+        listItem.classList.add('list-group-item');
+        list.appendChild(listItem);
+    });
+}
+
+function updateNameDisplay(name) {
+    var nameDisplay = document.getElementById('nameDisplay');
+    nameDisplay.textContent = name; // Setzt den aktuellen Namen
+    var nameDisplayContainer = document.getElementById('nameDisplayContainer');
+    nameDisplayContainer.style.animation = 'none'; // Setzt die Animation zurück
+    setTimeout(() => {
+      nameDisplayContainer.style.animation = ''; // Startet die Animation
+    }, 10); // Ein kleiner Timeout, um die Animation zurückzusetzen
+  }
